315d26b6061b889df6f49b70aba0655ad2ee9945
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-06-23 06:39:03 +0000

    rf_test working

--- function/012_rf_test.py
+++
@@ -1,2 +0,0 @@
-csv_file_path = '/content/drive/MyDrive/The Paper/Numerical/Code/training_data_v2.csv'
-train_cognitive_and_engagement_models(csv_file_path, 0.2, 20, 100)
---
+++ function/a2_rf_test.py
@@ -0,0 +1,4 @@
+from a1_rf_training import train_cognitive_and_engagement_models
+
+csv_file_path = 'data/sample_training_data.csv'
+train_cognitive_and_engagement_models(csv_file_path, 0.2, 20, 100)
